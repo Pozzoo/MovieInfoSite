e417d61ba0b82c55ccdba937b7977d788650cb29
pozzoo <arturpozzocmi@gmail.com>
2024-01-10 11:08:54 -0300

    updated movie info pages

--- movieDetailsScript.js
+++ movieDetailsScript.js
@@ -4,32 +4,41 @@ const imgPath = "https://image.tmdb.org/t/p/w1280";
 
 const movieImg = document.getElementById("movieImg")
 const movieTitle = document.getElementById("movieTitle");
+const movieTagline = document.getElementById("movieTagline")
+const movieVote = document.getElementById("movieVote")
 const movieOverview = document.getElementById("movieOverview");
 const siteTitle = document.getElementById("siteTitle");
 const divGenres = document.getElementById("genres");
+const movieReleaseDate = document.getElementById("movieReleaseDate")
+const movieRuntime = document.getElementById("movieRuntime")
+const movieBudget = document.getElementById("movieBudget")
+const movieRevenuemovieRevenue = document.getElementById("movieRevenue")
 
 const movieId = sessionStorage.getItem("movieId");
+var genresDisp = " ";
 console.log(movieId)
 
 movieSearch(movieSearchApi + movieId + "?api_key=" + apiKey)
 
 function movieSearch(url) {
 
-    console.log(url)
+    //console.log(url)
     fetch(url).then(res => res.json())
     .then(function(data) {
             data.genres.forEach(element => {
-            const genreText = document.createElement("p");
-            genreText.setAttribute("id", "genreText");
-
-            divGenres.appendChild(genreText);
-
-            genreText.innerText = `${element.name}`;
+            genresDisp = genresDisp +  `${element.name}` + ", ";
         });
         
         movieImg.src = imgPath + data.poster_path;
         siteTitle.innerText = `${data.original_title}` + " Info";
         movieTitle.innerText = `${data.original_title}`;
-        movieOverview.innerText = `${data.overview}`
+        movieTagline.innerText = `${data.tagline}`;
+        movieVote.innerText += Number(`${data.vote_average}`).toFixed(2);
+        movieOverview.innerText = `${data.overview}`;
+        genreText.innerText = " " + genresDisp;
+        movieReleaseDate.innerText += `${data.release_date}`;
+        movieRuntime.innerText += `${data.runtime}` + "min";
+        movieBudget.innerText += `${data.budget}`;
+        movieRevenuemovieRevenue.innerText += `${data.revenue}`;
     })
 }
